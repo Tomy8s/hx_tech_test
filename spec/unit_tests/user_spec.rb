834bfa8285978124eae1d5d747e48dd921899a60
Tomy8s <tomy8s@hotmail.com>
2017-01-04 10:30:18 +0000

    test #get_forename returns correctly

--- spec/unit_tests/user_spec.rb
+++ spec/unit_tests/user_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe User do
   let(:email) { 'test@test.com' }
-  let(:subject) { User.new({email: email}) }
+  let(:forename) { 'tom' }
+  let(:subject) { User.new({email: email, forename: forename}) }
   describe '#get_id' do
     it 'should return an integer as an id' do
       expect(subject.get_id).to be_a Fixnum
@@ -13,6 +14,12 @@ describe User do
     # email = 'test@test.com'
     it 'should return the email user in initialization' do
       expect(subject.get_email).to eq email
+    end
+  end
+
+  describe '#forename' do
+    it 'should return a capitalize version of the given forename' do
+      expect(subject.get_forename).to eq forename.capitalize
     end
   end
 end
