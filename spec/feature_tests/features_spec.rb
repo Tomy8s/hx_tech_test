adeb6f3c1107d8d120f585f4b93e41660b9f88e6
Tomy8s <tomy8s@hotmail.com>
2017-01-04 13:48:06 +0000

    added feature test for updating

--- spec/feature_tests/features_spec.rb
+++ spec/feature_tests/features_spec.rb
@@ -4,16 +4,23 @@ describe 'creating a user' do
   context 'no user specified to return' do
     it 'returns the last user' do
       visit 'http://localhost:4567/new?email=test@test.com&forename=tom&surname=yates'
-      expect(page).to have_content '"email":"test@test.com","forename":"Tom","surname":"Yates"'
+      expect(page).to have_content '{"id":1,"email":"test@test.com","forename":"Tom","surname":"Yates"'
     end
   end
 
   context 'user specified to return' do
     it 'returns the last user' do
-      n = 1
-      10.times{ Users.create({email: "test@test.com#{n}", forename: "tom#{n}", surname: "yates#{n}"}) }
+      10.times{ visit "http://localhost:4567/new?email=test@test.com&forename=tom&surname=yates" }
       visit 'http://localhost:4567/get?id=2'
-      expect(page).to have_content '"email":"test@test.com2","forename":"Tom2","surname":"Yates2"'
+      expect(page).to have_content '{"id":2,"email"'
     end
+  end
+end
+
+describe 'updating an entry' do
+  it 'updates an entry' do
+    visit 'http://localhost:4567/new?email=test@test.com&forename=tom&surname=yates'
+    visit 'http://localhost:4567/update?id=1&surname=smith'
+    expect(page).to have_content '"email":"test@test.com","forename":"Tom","surname":"Smith'
   end
 end
