c4556f34be6b6569c92db12a8127bff017a7cf7f
Tomy8s <tomy8s@hotmail.com>
2017-01-04 10:29:47 +0000

    #get_email returns correct email

--- lib/user.rb
+++ lib/user.rb
@@ -1,13 +1,18 @@
 class User
   @@registered_users = 0
 
-  def initialize
+  def initialize user_data
     @@registered_users += 1
     @id = @@registered_users
+    @email = user_data[:email] || nil
   end
 
   def get_id
     return_id = @id
     return_id
+  end
+
+  def get_email
+    @email.dup
   end
 end
