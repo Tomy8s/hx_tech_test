c2140c6c7ca80933e2ef2ae1233c38c10e611a54
Tomy8s <tomy8s@hotmail.com>
2017-01-04 10:27:09 +0000

    test for #get_email

--- spec/unit_tests/user_spec.rb
+++ spec/unit_tests/user_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe User do
+  let(:email) { 'test@test.com' }
+  let(:subject) { User.new({email: email}) }
   describe '#get_id' do
     it 'should return an integer as an id' do
       expect(subject.get_id).to be_a Fixnum
+    end
+  end
+
+  describe '#get_email' do
+    # email = 'test@test.com'
+    it 'should return the email user in initialization' do
+      expect(subject.get_email).to eq email
     end
   end
 end
