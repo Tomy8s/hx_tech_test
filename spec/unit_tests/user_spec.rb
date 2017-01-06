f04a9a9256ac580346f34afc7d0deca0cef6c145
Tomy8s <tomy8s@hotmail.com>
2017-01-06 13:08:45 +0000

    rearranged unit tests into more logical groups (#display_json)

--- spec/unit_tests/user_spec.rb
+++ spec/unit_tests/user_spec.rb
@@ -12,35 +12,34 @@ describe User do
     end
   end
 
-  describe '#get_email' do
-    # email = 'test@test.com'
+  describe '#display_json' do
     it 'should return the email user in initialization' do
       expect(subject.display_json.split(",")[1].split("\":")[1].tr('"','')).to eq email
     end
-  end
-
-  describe '#get_forename' do
+    
     it 'should return a capitalize version of the given forename' do
       expect(subject.display_json.split(",")[2].split("\":")[1].tr('"','')).to eq forename.capitalize
     end
-  end
-
-  describe '#get_surname' do
+    
     it 'should return a capitalize version of the given surname' do
       expect(subject.display_json.split(",")[3].split("\":")[1].tr('"','')).to eq surname.capitalize
     end
-  end
-
-  describe '#get_created' do
+    
     it 'should return the time and date of creation' do
       time_created = Time.now
       allow(Time).to receive(:now).and_return(time_created)
       expect(subject.display_json.split(",")[4].split("\":")[1].tr('"','')).to eq time_created.to_s
     end
+  
+    it 'should return null as update' do
+      time_updated = Time.now
+      allow(Time).to receive(:now).and_return(time_updated)
+      expect(subject.display_json.split(",")[5].split("\":")[1].tr('"','').tr('}','')).to eq "null"
+    end
   end
 
   describe '#update' do
-    it 'should update the email' do
+    it 'should update the email without affecting other atributes' do
       new_email = 'test2@test2.com'
       subject.update({email: new_email})
       expect(subject.display_json.split(",")[1].split("\":")[1].tr('"','')).to eq new_email
@@ -48,7 +47,7 @@ describe User do
       expect(subject.display_json.split(",")[3].split("\":")[1].tr('"','')).to eq surname.capitalize
     end
 
-    it 'should update the forename' do
+    it 'should update the forename without affecting other atributes' do
       new_forename = 'john'
       subject.update({forename: new_forename})
       expect(subject.display_json.split(",")[1].split("\":")[1].tr('"','')).to eq email
@@ -56,7 +55,7 @@ describe User do
       expect(subject.display_json.split(",")[3].split("\":")[1].tr('"','')).to eq surname.capitalize
     end
 
-    it 'should update the surname' do
+    it 'should update the surname without affecting other atributes' do
       new_surname = 'smith'
       subject.update({surname: new_surname})
       expect(subject.display_json.split(",")[1].split("\":")[1].tr('"','')).to eq email
@@ -64,7 +63,7 @@ describe User do
       expect(subject.display_json.split(",")[3].split("\":")[1].tr('"','')).to eq new_surname.capitalize
     end
 
-    it 'should set @update to current time' do
+    it 'should set @update to current time without affecting other atributes' do
       time_updated = Time.now
       allow(Time).to receive(:now).and_return(time_updated)
       subject.update({})
@@ -72,16 +71,6 @@ describe User do
       expect(subject.display_json.split(",")[2].split("\":")[1].tr('"','')).to eq forename.capitalize
       expect(subject.display_json.split(",")[3].split("\":")[1].tr('"','')).to eq surname.capitalize
       expect(subject.display_json.split(",")[5].split("\":")[1].tr('"','').tr('}','')).to eq time_updated.to_s
-    end
-  end
-
-  describe '#get_updated' do
-    context 'not updated' do
-      it 'should return nil' do
-        time_updated = Time.now
-        allow(Time).to receive(:now).and_return(time_updated)
-        expect(subject.display_json.split(",")[5].split("\":")[1].tr('"','').tr('}','')).to eq "null"
-      end
     end
   end
 end
